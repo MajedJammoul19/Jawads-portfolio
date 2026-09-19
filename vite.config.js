d9e0f9e5d8d58aff0478ac87e15926fbd415fb6e
MajedJammoul19 <majdjammoul89@gmail.com>
2026-09-19 15:57:21 +0300

    fixing vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,19 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 export default defineConfig({
   plugins: [
     react(),
-    ViteImageOptimizer({
-      jpg: { quality: 75 },
-      jpeg: { quality: 75 },
-      png: { quality: 75 },
-      webp: { quality: 75 },
-    }),
+  ViteImageOptimizer({
+  jpg: { quality: 75 },
+  jpeg: { quality: 75 },
+  png: { quality: 75 },
+  webp: { quality: 75 },
+  test: /\.(jpe?g|png|webp)$/i,
+  includePublic: true,
+  logStats: true,
+  ansiColors: true,
+  svg: {},
+  png: {},
+  jpg: { quality: 75 },
+  jpeg: { quality: 75 },
+}),
   ],
 })
